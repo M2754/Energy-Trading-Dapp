13dff2b9ceaa079a70721ad292fbe62de485c187
unp18 <156875884+unp18@users.noreply.github.com>
2025-02-02 13:03:56 +0530

    Add files via upload

---
+++ src/components/ConsumerPortal.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import OfferList from './OfferList';
+import EnergyDisplay from './EnergyDisplay';
+import CarbonCreditDisplay from './CarbonCreditDisplay';
+import CarbonCreditOfferList from './CarbonCreditOfferList';
+import Loading from './Loading';
+
+const ConsumerPortal = () => {
+    const [loading, setLoading] = useState(false);
+
+    return (
+        <div>
+             {loading ? <Loading/> :
+            <>
+             <h2>Consumer Portal</h2>
+            <EnergyDisplay />
+            <CarbonCreditDisplay/>
+             <OfferList />
+             <CarbonCreditOfferList/>
+            
+            </>
+             }
+        </div>
+    );
+};
+
+export default ConsumerPortal;
